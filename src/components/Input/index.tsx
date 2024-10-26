ed5215d83251a69e6bcde8e1687b9917a997fbb7
Rootjang92 <janggeunho7@gmail.com>
2024-10-26 16:58:00 +0900

    Style: input component style 조정

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,13 +7,22 @@ interface Props {
 }
 
 const Input = ({ value, onChange, onEnter }: Props) => {
-  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       onEnter();
     }
   };
 
-  return <input type="text" value={value} onChange={onChange} placeholder="할 일을 입력해 주세요." />
+  return (
+    <input 
+      className="w-full p-[32px] mb-6 rounded-3xl bg-[#E5E5E5] mt-10 text-[20px] placeholder:text-[#B9B9B9] focus:outline-none" 
+      type="text" 
+      value={value} 
+      onChange={onChange}
+      onKeyDown={handleKeyDown}
+      placeholder="할 일을 입력해 주세요." 
+    />
+  )
 };
 
 export default Input;
--- src/components/pages/TodoUserListPage.tsx
+++ src/components/pages/TodoUserListPage.tsx
@@ -1,11 +1,41 @@
 "use client";
+import { ChangeEvent, useState } from "react";
+
+import Input from "../Input";
 
 interface Props {}
 
+interface Todo {
+  id: number,
+  todo: string;
+  done: boolean;
+}
+
 const TodoUserListPage = ({}: Props) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todoValue, setTodoValue] = useState('');
+
+  const handleChangeTodoValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    const newTodo: Todo = {
+      id: Date.now(),
+      todo: todoValue,
+      done: false
+    };
+
+    setTodos([...todos, newTodo]);
+    setTodoValue('');
+  }
+
+  console.log(todos);
+
   return (
     <section className="flex flex-col items-center justify-center font-pretendard">
       <h1 className="text-6xl font-bold pt-10 text-[#333333]">To Do List</h1>
+      <Input value={todoValue} onChange={handleChangeTodoValue} onEnter={handleAddTodo} />
     </section>
   );
 };
